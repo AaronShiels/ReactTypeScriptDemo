c4fb98d2d2e336ad57b126738759ac7d65f4de4b
Aaron Shiels <aaron.shiels@outlook.com>
2018-03-08 23:08:30 +1000

    Hello world React app working.

---
+++ src/ReactTypeScriptDemo.App/ClientApp/app.tsx
@@ -0,0 +1,8 @@
+﻿import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Hello from "./hello";
+
+ReactDOM.render(
+    <Hello name="Aaron" />,
+    document.getElementById("root")
+);
---
+++ src/ReactTypeScriptDemo.App/webpack.config.js
@@ -0,0 +1,22 @@
+﻿var path = require("path");
+
+module.exports = {
+    entry: ["./ClientApp/app.tsx"],
+    output: {
+        path: path.resolve(__dirname, "wwwroot"),
+        filename: "bundle.js"
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/
+            }
+        ]
+    }
+};
